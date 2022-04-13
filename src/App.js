e73b221db6a2425e745add71c4a6ae9b0f5c453f
Harrison365 <harrisonroyds@yahoo.co,uk>
2022-04-13 15:05:16 +0100

    tidy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
 }
 
 export default App;
